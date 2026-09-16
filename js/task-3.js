25b9a8fd86cb9ed0e56b9d6cb49e28aa8d9ecb27
Max Didyk <maximderen516@gmail.com>
2026-09-16 23:13:58 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -9,7 +9,7 @@ const profile = {
         this.playTime += hours;
     },
     getInfo(){
-        return `${this.username} має ${this.playTime} активних годин!`;  
+        return `${this.username} has  ${this.playTime} active hours!`;  
     }
 }
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
